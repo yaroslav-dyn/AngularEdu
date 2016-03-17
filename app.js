ec7cdebdc30d692f739358c7fcbfbe0d47988a28
yaroslav-dyn <norman_zp@outlook.com>
2016-03-17 18:17:09 +0200

    anycode

--- app.js
+++ app.js
@@ -2,8 +2,22 @@
 	var app = angular.module('testMod',[]);
 
     //gems
-	app.controller('testController',function(){
+	app.controller('testController',function($scope){
 
+		$scope.imgVectors = [
+			{
+				"name": "acid",
+				"image":"images/medical.svg" 
+			},
+			{
+				"name": "acid second",
+				"image":"images/medical.svg" 
+			},
+					{
+				"name": "acid last",
+				"image":"images/medical.svg" 
+			}
+		];
 		
 	});
 
